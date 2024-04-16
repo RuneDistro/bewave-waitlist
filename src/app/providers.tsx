5611354bafa8d3ae694331c754250a4003b241fd
Pietro Rhyan <pietroqjg@gmail.com>
2024-04-15 22:23:04 -0300

    feat: add analytics and speed insights

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import { Toaster } from 'sonner'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Toaster />
       {children}
+      <Analytics />
+      <SpeedInsights />
     </>
   )
 }
