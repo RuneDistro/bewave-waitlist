3d9c0fcad068ad4b6298efea65f16692468429db
Pietro Rhyan <pietroqjg@gmail.com>
2024-10-15 10:52:05 -0300

    fix: change the cta button link

--- src/components/cta-button.tsx
+++ src/components/cta-button.tsx
@@ -9,7 +9,7 @@ export function CTAButton() {
   return (
     <div className="button-wrapper">
       <button
-        onClick={() => router.push('https://tally.so/r/w2rqgp')}
+        onClick={() => router.push('https://contate.me/bewave')}
         className="w-[calc(100%-2px)] h-[calc(100%-2px)] absolute z-30 top-[1px] left-[1px] text-sm bg-black flex items-center justify-center gap-5 rounded-3xl focus:outline-none"
       >
         Distribua
